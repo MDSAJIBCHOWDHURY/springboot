5a1b84f2bc53f88dfe413d78fdd6e53f22540e23
MDSAJIBCHOWDHURY <62007928+MDSAJIBCHOWDHURY@users.noreply.github.com>
2022-08-23 14:12:47 +0600

    native sql query

--- src/main/java/com/tahmid/springboot/controller/EmployeeController.java
+++ src/main/java/com/tahmid/springboot/controller/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,44 +19,71 @@ public class EmployeeController {
     private EmployeeRepository employeeRepository;
 
     //get all employees
-    @RequestMapping(value="/get", method=RequestMethod.GET)
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
     public List<Employee> getAllEmployees() {
-        return employeeRepository.findAll();
+
+        //return employeeRepository.findAll();
+        return employeeRepository.findAllEmployee();
     }
 
     //create employee
-    @RequestMapping(value="/insert", method=RequestMethod.POST)
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
     public Employee createEmployee(@RequestBody Employee employee) {
+
         return employeeRepository.save(employee);
+
     }
 
     //get employee by id
-    @RequestMapping(value = "/get/{id}",method = RequestMethod.GET)
-    public ResponseEntity<Employee> getEmployeeById(@PathVariable long id) {
-        Employee employee = employeeRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:" + id));
-        return ResponseEntity.ok(employee);
+    @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
+    public Employee getEmployeeById(@PathVariable long id) {
+//        Employee employee = employeeRepository.findById(id)
+//                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:" + id));
+//        return ResponseEntity.ok(employee);
+        Employee employee = employeeRepository.findEmployee(id);
+        return employee;
     }
 
     //update employee by id
-    @RequestMapping(value = "/update/{id}",method = RequestMethod.POST)
-    public ResponseEntity<Employee> updateEmployee(@PathVariable long id, @RequestBody Employee employeeDetails) {
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
+    public Employee updateEmployee(@PathVariable long id, @RequestBody Employee employeeDetails) {
         Employee updateEmployee = employeeRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id = " + id));
-        updateEmployee.setFirstName(employeeDetails.getFirstName());
-        updateEmployee.setLastName(employeeDetails.getLastName());
-        updateEmployee.setEmailId(employeeDetails.getEmailId());
-        employeeRepository.save(updateEmployee);
-        return ResponseEntity.ok(updateEmployee);
+//        updateEmployee.setFirstName(employeeDetails.getFirstName());
+//        updateEmployee.setLastName(employeeDetails.getLastName());
+//        updateEmployee.setEmailId(employeeDetails.getEmailId());
+//      employeeRepository.save(updateEmployee);
+        System.out.println("FName ========= " + employeeDetails.getLastName());
+
+        employeeRepository.updateEmployee(employeeDetails.getFirstName(), employeeDetails.getLastName(),
+                employeeDetails.getEmailId(), id);
+
+        return employeeDetails;
     }
 
     //delete employee
-    @RequestMapping(value = "/delete/{id}",method = RequestMethod.DELETE)
-    public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable long id) {
-        Employee employee = employeeRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:" + id));
-        employeeRepository.delete(employee);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    public void deleteEmployee(@PathVariable long id) {
+//        Employee employee = employeeRepository.findById(id)
+//                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:" + id));
+//        employeeRepository.delete(employee);
+        employeeRepository.deleteEmployee(id);
+
+
+
+    }
+
+    //get json objects
+    @RequestMapping(value = "/objects",method = RequestMethod.GET)
+    public List<Employee> getJsonObjects(@RequestBody List<Employee> employees){
+        List<Employee> updatedEmployees = new ArrayList<>();
+        updatedEmployees.addAll(employees);
+        for(int i=0;i<updatedEmployees.size();i++){
+            Employee employee = updatedEmployees.get(i);
+            employee.setId(100+i);
+        }
+        return updatedEmployees;
+
     }
 }
 
--- src/main/java/com/tahmid/springboot/repository/EmployeeRepository.java
+++ src/main/java/com/tahmid/springboot/repository/EmployeeRepository.java
@@ -3,9 +3,32 @@ package com.tahmid.springboot.repository;
 
 import com.tahmid.springboot.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+import javax.transaction.Transactional;
+import java.util.List;
 
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    @Query(value = "select * from employees ", nativeQuery = true)
+    List<Employee> findAllEmployee();
+
+    @Query(value = "select * from employees e where e.id = :id", nativeQuery = true)
+    Employee findEmployee(@Param("id") Long id);
+
+
+    @Query(value = "update employees e set e.first_name=:firstName,e.last_name=:lastName," +
+            "e.email_id=:emailId where e.id= :id",nativeQuery = true)
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    void updateEmployee(@Param("firstName") String firstName,@Param("lastName")String lastName,
+                       @Param("emailId") String emailId,@Param("id") Long id);
+
+    @Query(value = "delete from employees where id =:id",nativeQuery = true)
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    void deleteEmployee(@Param("id") Long id);
 }
